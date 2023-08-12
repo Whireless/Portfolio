f9de88f1426cfd62ed7b628090be64d4a110af46
Александр <alextrexen@gmail.com>
2023-08-12 15:02:47 +0300

    Переработан слайдер с проектами

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,22 +1,25 @@
 const page = document.querySelector('body');
 
+const themeButton = page.querySelector('.main-header__theme-toggle');
+
 const mainNav = page.querySelector('.main-nav'),
       navBtn = mainNav.querySelector('.main-nav__nav-button'),
       navElem = mainNav.querySelectorAll('.main-nav__nav-item a'),
       openMe = mainNav.querySelector('.main-nav__title');
 
-const themeButton = page.querySelector('.main-header__theme-toggle');
+const about = page.querySelector('.about-me'),
+      shortInfoBtn = about.querySelector('.about-me__button--short'),
+      moreInfoBtn = about.querySelector('.about-me__button--more'),
+      infoClose = about.querySelectorAll('.about-me__button-close'),
+      shortInfo = about.querySelector('.about-me__shorter-info'),
+      moreInfo = about.querySelector('.about-me__more-info');
 
-const shortInfoBtn = page.querySelector('.about-me__button--short'),
-      moreInfoBtn = page.querySelector('.about-me__button--more'),
-      infoClose = page.querySelectorAll('.about-me__button-close'),
-      shortInfo = page.querySelector('.about-me__shorter-info'),
-      moreInfo = page.querySelector('.about-me__more-info');
-
-const projectImg = page.querySelector('.portfolio__project-image'),
-      projectLink = page.querySelector('.portfolio__project-link'),
-      prevProject = page.querySelector('.portfolio__button--previus'),
-      nextProject = page.querySelector('.portfolio__button--next');
+const portfolio = page.querySelector('.portfolio'),
+      projectImg = portfolio.querySelector('.portfolio__project-image'),
+      projectLink = portfolio.querySelector('.portfolio__project-link'),
+      projectType = portfolio.querySelector('.portfolio__project-type span'),
+      prevProject = portfolio.querySelector('.portfolio__button--previus'),
+      nextProject = portfolio.querySelector('.portfolio__button--next');
 
 const buttonUp = page.querySelector('.main-footer__button-up');
 
@@ -100,57 +103,117 @@ window.addEventListener('keydown', (evt) => {
 });
 
 // Список проектов
+// const projects = [
+//   {
+//     img: 'img/projects/Andrestatix.png',
+//     alt: 'Если хочешь стать греческим атлетом - тебе сюда.',
+//     href: 'https://andrestatix.com',
+//     text: 'Лучший сайт по развитию своего тела',
+//     type: 'Коммерческий',
+//   },
+//   {
+//     img: 'img/projects/Nevelvend.png',
+//     alt: 'Лучшие палочки в мире. И это точно.',
+//     href: 'https://whireless.github.io/Nevelvend/',
+//     text: 'Твоё удобство на кончике размешивателя',
+//     type: 'Учебный',
+//   },
+//   {
+//     img: 'img/projects/ProductFit.png',
+//     alt: 'На самом деле интернет создали мы.',
+//     href: 'https://whireless.github.io/ProductFit/',
+//     text: 'Разработка ИТ-продуктов нового поколения',
+//     type: 'Учебный',
+//   },
+//   {
+//     img: 'img/projects/Pink.png',
+//     alt: 'Надоели серые будни? Тогда мы идём к вам!',
+//     href: 'https://whireless.github.io/Pink/',
+//     text: 'Приложение для скрашивания скучных дней',
+//     type: 'Учебный',
+//   },
+//   {
+//     img: 'img/projects/Restaurant.png',
+//     alt: 'Путник, ты слишком долго искал сочный стейк, в этом ресторане твоя награда',
+//     href: 'https://whireless.github.io/Restaurant/',
+//     text: 'Шикарный ужин в Санкт-Петербурге',
+//     type: 'Учебный',
+//   },
+//   {
+//     img: 'img/projects/Grand-Theatre.png',
+//     alt: 'Надо уважать культуру, мать её.',
+//     href: 'https://whireless.github.io/Grand-Theatre/',
+//     text: 'Большой театр в Санкт-Петербурге',
+//     type: 'Учебный',
+//   },
+//   {
+//     img: 'img/projects/Nerds.png',
+//     alt: 'Они создали мой внеземной дизайн',
+//     href: 'https://whireless.github.io/Nerds/',
+//     text: 'Разработка дизайна для сайтов и приложений',
+//     type: 'Учебный',
+//   },
+// ];
+
 const projects = [
   {
     img: 'img/projects/Andrestatix.png',
     alt: 'Если хочешь стать греческим атлетом - тебе сюда.',
-    href: 'https://whireless.github.io/andrestatix/',
-    text: 'Лучший сайт по развитию своего тела',
+    href: 'https://andrestatix.com',
+    text: 'andrestatix.com',
+    type: 'Коммерческий',
   },
   {
     img: 'img/projects/Nevelvend.png',
     alt: 'Лучшие палочки в мире. И это точно.',
     href: 'https://whireless.github.io/Nevelvend/',
-    text: 'Твоё удобство на кончике размешивателя',
+    text: 'Nevelvend',
+    type: 'Учебный',
   },
   {
     img: 'img/projects/ProductFit.png',
     alt: 'На самом деле интернет создали мы.',
     href: 'https://whireless.github.io/ProductFit/',
-    text: 'Разработка ИТ-продуктов нового поколения',
+    text: 'ProductFit',
+    type: 'Учебный',
   },
   {
     img: 'img/projects/Pink.png',
     alt: 'Надоели серые будни? Тогда мы идём к вам!',
     href: 'https://whireless.github.io/Pink/',
-    text: 'Приложение для скрашивания скучных дней',
+    text: 'Pink',
+    type: 'Учебный',
   },
   {
     img: 'img/projects/Restaurant.png',
     alt: 'Путник, ты слишком долго искал сочный стейк, в этом ресторане твоя награда',
     href: 'https://whireless.github.io/Restaurant/',
-    text: 'Шикарный ужин в Санкт-Петербурге',
+    text: 'Restaurant',
+    type: 'Учебный',
   },
   {
     img: 'img/projects/Grand-Theatre.png',
     alt: 'Надо уважать культуру, мать её.',
     href: 'https://whireless.github.io/Grand-Theatre/',
-    text: 'Большой театр в Санкт-Петербурге',
+    text: 'Grand-Theatre',
+    type: 'Учебный',
   },
   {
     img: 'img/projects/Nerds.png',
     alt: 'Они создали мой внеземной дизайн',
     href: 'https://whireless.github.io/Nerds/',
-    text: 'Разработка дизайна для сайтов и приложений',
+    text: 'Nerds',
+    type: 'Учебный',
   },
 ];
 
 const getPreviewProject = () => {
-  const {img, alt, href, text} = projects[i];
+  const {img, alt, href, text, type} = projects[i];
   projectImg.src = img;
   projectImg.alt = alt;
   projectLink.href = href;
   projectLink.textContent = text;
+  projectType.textContent = type;
   return;
 };
 
@@ -158,14 +221,18 @@ let i = 0;
 const checkIndex = () => {
   if (i < projects.length - 1) {
     nextProject.disabled = false;
+    nextProject.style.opacity = 1;
   } else {
     nextProject.disabled = true;
+    nextProject.style.opacity = 0;
   };
 
   if (i > 0) {
     prevProject.disabled = false;
+    prevProject.style.opacity = 1;
   } else {
     prevProject.disabled = true;
+    prevProject.style.opacity = 0;
   };
   return;
 };
